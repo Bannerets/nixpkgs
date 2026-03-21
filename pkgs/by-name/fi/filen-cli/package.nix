9dbbd978ee72abe7a6e865edce5415e2000f9125
eilvelia <hi@eilvelia.cat>
2026-03-21 13:29:02 +0000

    wip

--- pkgs/by-name/fi/filen-cli/package.nix
+++ pkgs/by-name/fi/filen-cli/package.nix
@@ -58,6 +58,8 @@ let
     outputHash =
       {
         aarch64-darwin = "sha256-+nTCiCEXsEz7YqRZkHlP3CWL7e7OPdds33BTwfqTL5c=";
+        aarch64-linux = lib.fakeHash;
+        x86_64-darwin = lib.fakeHash;
         x86_64-linux = "sha256-Ky+ewpdd5nKvRzXwEAUgT7P/OW3v6fdb2r6SGxZ/JEc=";
       }
       .${stdenv.hostPlatform.system}
